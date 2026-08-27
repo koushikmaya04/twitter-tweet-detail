2ab711a1bf6cca1849bf9ee731d971970cb2f091
Koushik Maya <koushikmaya04@gmail.com>
2026-08-27 15:32:36 +0530

    feat: implement post model classes

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,12 @@
 import debounce from "./search/debounce.js";
 import "./likes/likes.js";
+import {
+    Tweet,
+    Comment,
+    Retweet
+} from "./models/post.js";
+
+
 
 const searchInput = document.querySelector("#search-input");
 
@@ -11,4 +18,51 @@ const debouncedSearch = debounce(handleSearch, 500);
 
 searchInput.addEventListener("input", (event) => {
     debouncedSearch(event.target.value);
-});
+}); 
+ 
+// ================================
+// Post Model
+// ================================
+
+const tweet = new Tweet(
+    1,
+    "Koushik Maya",
+    "Learning JavaScript classes",
+    10
+);
+
+console.log("Tweet:", tweet);
+console.log("Tweet likes:", tweet.likes);
+
+tweet.like();
+
+console.log("After like:", tweet.likes);
+
+tweet.unlike();
+
+console.log("After unlike:", tweet.likes);
+
+
+const comment = new Comment(
+    2,
+    "Alex Developer",
+    "Great explanation!",
+    3
+);
+
+console.log("Comment:", comment);
+console.log("Comment type:", comment.type);
+console.log("Comment likes:", comment.likes);
+
+
+const retweet = new Retweet(
+    3,
+    "Sam",
+    tweet,
+    5
+);
+
+console.log("Retweet:", retweet);
+console.log("Retweet type:", retweet.type);
+console.log("Retweet content:", retweet.content);
+console.log("Retweet likes:", retweet.likes);
