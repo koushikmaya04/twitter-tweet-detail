fc38f6443df1ea9851bcfb2432fe82f77cba109e
koushikmaya04 <koushikmaya04@gmail.com>
2026-08-31 16:46:30 +0530

    Refactor filter, sort, and dedupe functions
    Refactor functions for improved readability by formatting arrow functions and reducing lines.

--- javascript/feed/pipeline.js
+++ javascript/feed/pipeline.js
@@ -1,22 +1,37 @@
 function filterByFollowing(posts, following) {
-    return posts.filter(post => following.includes(post.author));
-}
-
-function sortByRecency(posts) {
-    return [...posts].sort(
-        (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+    return posts.filter(
+        (post) => following.includes(post.author)
     );
 }
 
-function dedupe(posts) {
-    return posts.reduce((uniquePosts, post) => {
-        if (!uniquePosts.some(item => item.id === post.id)) {
-            uniquePosts.push(post);
-        }
 
-        return uniquePosts;
-    }, []);
+function sortByRecency(posts) {
+    return [...posts].sort(
+        (a, b) =>
+            new Date(b.createdAt) -
+            new Date(a.createdAt)
+    );
 }
+
+
+function dedupe(posts) {
+    return posts.reduce(
+        (uniquePosts, post) => {
+
+            if (
+                !uniquePosts.some(
+                    (item) => item.id === post.id
+                )
+            ) {
+                uniquePosts.push(post);
+            }
+
+            return uniquePosts;
+        },
+        []
+    );
+}
+
 
 function pipe(...functions) {
     return function (initialValue) {
@@ -26,6 +41,7 @@ function pipe(...functions) {
         );
     };
 }
+
 
 export {
     filterByFollowing,
